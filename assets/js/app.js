cc9b24b3d06387aad1314c7b242815ba96c656d5
ShailaShaimunDiba <shaimunshaila.97@gmail.com>
2025-01-26 23:38:25 +0600

    second_commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,4 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
     setInterval(() => {
         timeStamp.innerHTML = TimeStamp(false, false, true);
     }, 1000);
+
+
+    let toggle_btn = document.getElementById('toggle_btn');
+    toggle_btn.onclick = () => {
+        let menu = document.querySelector(".my_navbar ul");
+        menu.classList.toggle("active");
+        let icon = document.querySelector(".toggle_btn .icon");
+
+        if (icon.classList.contains("fa-bars")) {
+            icon.classList.remove("fa-bars");
+            icon.classList.add("fa-bars-staggered");
+        } else {
+            icon.classList.remove("fa-bars-staggered");
+            icon.classList.add("fa-bars");
+        }
+    }
 });
+
